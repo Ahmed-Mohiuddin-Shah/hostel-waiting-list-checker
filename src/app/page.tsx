31b58d115baa019fae845f06f6e48b3611726aa4
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-29 14:39:14 +0500

    chore: Update react-icons package to include FaBackward icon and refactor modal logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
   ModalFooter,
   useDisclosure,
 } from "@nextui-org/modal";
-import { FaArrowRight, FaSpinner } from "react-icons/fa6";
+import { FaArrowRight, FaBackward, FaSpinner } from "react-icons/fa6";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -19,9 +19,9 @@ import { apiURL } from "../../constants";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [showData, setShowData] = useState(false);
 
-  const [modalData, setModalData] = useState<StudentWaitInfo>({
+  const [studentData, setStudentData] = useState<StudentWaitInfo>({
     student_sr_no: 0,
     student_name: "",
     student_cms_id: 0,
@@ -57,18 +57,20 @@ export default function Home() {
       );
 
       // Ensure response is valid and contains data
-      if (response.data) {
-        setModalData({
+      if (response.data.status) {
+        setStudentData({
           student_sr_no: response.data.data.student_sr_no,
           student_name: response.data.data.student_name,
           student_cms_id: response.data.data.student_cms_id,
         });
-        console.log(modalData);
-        onOpen(); // Open the modal after setting the data
+        setShowData(true);
+        toast.success("Data found successfully.");
       } else {
         toast.error("No data found for the provided CMS ID.");
-      }
+        setShowData(false);
+      } // Handle error if no data is found
     } catch (error) {
+      setShowData(false);
       toast.error("An error occurred. Please try again later.");
     } finally {
       setIsLoading(false); // Ensure loading state is always reset
@@ -78,68 +80,79 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <Modal
-          isOpen={isOpen}
-          onClose={onClose}
-          placement="center"
-          isDismissable={false}
-        >
-          <ModalContent className="relative w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
-            <ModalHeader className="text-black text-4xl">Waiting Info</ModalHeader>
-            <ModalBody>
-              <div className="flex flex-col gap-3 text-black">
-                <div>
-                  <span className="font-bold">Student Name:</span>{" "}
-                  {modalData.student_name}
+        {showData ? (
+          <>
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+              <h1 className="mt-3 text-9xl">YOUR WAITING LIST POSITION IS</h1>
+              <p className="mt-3 text-9xl">
+                <b>{studentData.student_sr_no}</b>
+              </p>
+              <div className="mt-8 gap-10 text-white">
+                <div className="">
+                  <div className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black mb-5">
+                    <div className="flex flex-row justify-between text-white">
+                      <div>Student CMS ID</div>
+                      <div>{studentData.student_cms_id}</div>
+                    </div>
+                  </div>
                 </div>
                 <div>
-                  <span className="font-bold">Student CMS ID:</span>{" "}
-                  {modalData.student_cms_id}
+                  <div className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black">
+                    <div className="flex flex-row justify-between text-white">
+                      <div>Student Name</div>
+                      <div>{studentData.student_name}</div>
+                    </div>
+                  </div>
                 </div>
-                <div className="center">
-                  <span className="font-bold text-3xl">Waiting #</span>{" "}
+                <div>
+                  <button
+                    type="button"
+                    className="mt-4 p-3 w-96 text-lg bg-blue-500 text-white rounded-md flex flex-row justify-between"
+                    onClick={() => setShowData(false)}
+                  >
+                    <div>Check Another</div>
+                    <div>
+                      <FaBackward />
+                    </div>
+                  </button>
                 </div>
-                <div>{modalData.student_sr_no}</div>
               </div>
-            </ModalBody>
-            <ModalFooter>
-              <button
-                className="p-3 w-full text-lg bg-blue-500 text-white rounded-md"
-                onClick={onClose}
-              >
-                Close
-              </button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">Hostel Waiting List</h1>
-          <p className="mt-3 text-2xl">
-            Check Your Hostel Waiting List Position
-          </p>
-          <form className="mt-8 gap-5">
-            <div>
-              <input
-                type="number"
-                placeholder="Enter your CMS ID"
-                className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black"
-              />
-            </div>
-            <div>
-              <button
-                type="button"
-                className="mt-4 p-3 w-96 text-lg bg-blue-500 text-white rounded-md flex flex-row justify-between"
-                onClick={handleSubmit}
-              >
-                <div>Check</div>
+            </main>
+          </>
+        ) : (
+          <>
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+              <h1 className="text-6xl font-bold">Hostel Waiting List</h1>
+              <p className="mt-3 text-2xl">
+                Check Your Hostel Waiting List Position
+              </p>
+              <form className="mt-8 gap-5" onSubmit={(e)=>{
+                e.preventDefault();
+                handleSubmit();
+              }}>
                 <div>
-                  {isLoading && <FaSpinner className="animate-spin" />}
-                  {!isLoading && <FaArrowRight />}
+                  <input
+                    type="number"
+                    placeholder="Enter your CMS ID"
+                    className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black"
+                  />
                 </div>
-              </button>
-            </div>
-          </form>
-        </main>
+                <div>
+                  <button
+                    type="submit"
+                    className="mt-4 p-3 w-96 text-lg bg-blue-500 text-white rounded-md flex flex-row justify-between"
+                  >
+                    <div>Check</div>
+                    <div>
+                      {isLoading && <FaSpinner className="animate-spin" />}
+                      {!isLoading && <FaArrowRight />}
+                    </div>
+                  </button>
+                </div>
+              </form>
+            </main>
+          </>
+        )}
         <ToastContainer />
       </div>
     </>
