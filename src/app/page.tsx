d4b3ca8ce40f7fb555ade50136a7af37654bdf3d
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-29 13:01:51 +0500

    chore: Refactor form submission logic and add error handling for invalid CMS ID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,66 @@ import { FaArrowRight, FaSpinner } from "react-icons/fa6";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { StudentWaitInfo } from "../../types";
+import axios from "axios";
+import { apiURL } from "../../constants";
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const handleSubmit = () => {
-    setIsLoading(true);
-    onOpen();
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Simulate loading delay
-    toast.success("Your position in the waiting list is 5.", 
-      {
-        position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      }
-    );
+  const [modalData, setModalData] = useState<StudentWaitInfo>({
+    student_sr_no: 0,
+    student_name: "",
+    student_cms_id: 0,
+  });
 
+  const handleSubmit = async () => {
+    setIsLoading(true);
+
+    const cmsId = document.querySelector("input")?.value;
+    console.log(cmsId);
+
+    if (!cmsId) {
+      toast.error("Please enter a valid CMS ID.");
+      setIsLoading(false);
+      return;
+    }
+
+    // validate cmsId
+    if (
+      cmsId.length > 11 ||
+      cmsId.length < 6 ||
+      isNaN(parseInt(cmsId)) ||
+      parseInt(cmsId) < 0
+    ) {
+      toast.error("Please enter a valid CMS ID.");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `${apiURL}/student/student-waiting-info/${parseInt(cmsId)}`
+      );
+
+      // Ensure response is valid and contains data
+      if (response.data) {
+        setModalData({
+          student_sr_no: response.data.data.student_sr_no,
+          student_name: response.data.data.student_name,
+          student_cms_id: response.data.data.student_cms_id,
+        });
+        console.log(modalData);
+        onOpen(); // Open the modal after setting the data
+      } else {
+        toast.error("No data found for the provided CMS ID.");
+      }
+    } catch (error) {
+      toast.error("An error occurred. Please try again later.");
+    } finally {
+      setIsLoading(false); // Ensure loading state is always reset
+    }
   };
 
   return (
@@ -45,12 +83,24 @@ export default function Home() {
           onClose={onClose}
           placement="center"
           isDismissable={false}
-          className="flex items-center justify-center"
         >
           <ModalContent className="relative w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
-            <ModalHeader>Hostel Waiting List</ModalHeader>
+            <ModalHeader className="text-black text-4xl">Waiting Info</ModalHeader>
             <ModalBody>
-              <p>Your position in the waiting list is 5.</p>
+              <div className="flex flex-col gap-3 text-black">
+                <div>
+                  <span className="font-bold">Student Name:</span>{" "}
+                  {modalData.student_name}
+                </div>
+                <div>
+                  <span className="font-bold">Student CMS ID:</span>{" "}
+                  {modalData.student_cms_id}
+                </div>
+                <div className="center">
+                  <span className="font-bold text-3xl">Waiting #</span>{" "}
+                </div>
+                <div>{modalData.student_sr_no}</div>
+              </div>
             </ModalBody>
             <ModalFooter>
               <button
@@ -70,7 +120,7 @@ export default function Home() {
           <form className="mt-8 gap-5">
             <div>
               <input
-                type="text"
+                type="number"
                 placeholder="Enter your CMS ID"
                 className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black"
               />
@@ -90,6 +140,7 @@ export default function Home() {
             </div>
           </form>
         </main>
+        <ToastContainer />
       </div>
     </>
   );
