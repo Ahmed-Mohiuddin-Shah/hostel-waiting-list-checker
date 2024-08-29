9996a5ce5413dc18b967e5a278a16b1d9c526605
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-29 10:48:28 +0500

    chore: Add toast notifications and modal for hostel waiting list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,67 @@
 "use client";
 
 import { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
 import { FaArrowRight, FaSpinner } from "react-icons/fa6";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleSubmit = () => {
     setIsLoading(true);
+    onOpen();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Simulate loading delay
+    toast.success("Your position in the waiting list is 5.", 
+      {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }
+    );
+
   };
 
   return (
     <>
-      {/* page for student to enter their CMS ID */}
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          placement="center"
+          isDismissable={false}
+          className="flex items-center justify-center"
+        >
+          <ModalContent className="relative w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
+            <ModalHeader>Hostel Waiting List</ModalHeader>
+            <ModalBody>
+              <p>Your position in the waiting list is 5.</p>
+            </ModalBody>
+            <ModalFooter>
+              <button
+                className="p-3 w-full text-lg bg-blue-500 text-white rounded-md"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
         <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
           <h1 className="text-6xl font-bold">Hostel Waiting List</h1>
           <p className="mt-3 text-2xl">
@@ -24,12 +72,12 @@ export default function Home() {
               <input
                 type="text"
                 placeholder="Enter your CMS ID"
-                className="p-3 w-96 text-lg border border-gray-300 rounded-md"
+                className="p-3 w-96 text-lg border border-gray-300 rounded-md text-black"
               />
             </div>
             <div>
               <button
-                type="submit"
+                type="button"
                 className="mt-4 p-3 w-96 text-lg bg-blue-500 text-white rounded-md flex flex-row justify-between"
                 onClick={handleSubmit}
               >
